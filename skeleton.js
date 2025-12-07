d6c3c27cd212429f8cfae9f080c9c3dde5fd8e2d
AdsMet <metaadsmanagera1@gmail.com>
2025-12-07 18:51:41 +0500

    nothing

--- skeleton.js
+++
@@ -1,9 +0,0 @@
-// Wait until everything (CSS, fonts, images) is fully loaded
-window.addEventListener("load", () => {
-  const skeleton = document.getElementById("skeleton-loader");
-  if (skeleton) {
-    skeleton.classList.add("hidden");
-    // Remove after fade animation
-    setTimeout(() => skeleton.remove(), 800);
-  }
-});
